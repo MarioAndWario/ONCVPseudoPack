b486267e3227833c828e6476240fae34f981c3ee
wu2meng3 <wu1meng2@berkeley.edu>
2020-10-11 21:44:36 -0700

    home-make In with semicores, parameters from Cd

--- foundary/PBE_new/AssemblePP.sh
+++ foundary/PBE_new/AssemblePP.sh
@@ -2,11 +2,11 @@
 # This script extract the input for ONCVPSP from SG15 PP library (v1.2)
 # http://www.quantum-simulation.org/potentials/sg15_oncv/
 # and use ONCVPSP v3.3.1 + libxc v3.0.1 to produce ONCVPSP PBE pseudopotentials (SR+FR)
-# and store the pseudopotentials in /global/homes/m/mwu/PP/ONCVPSP/Abinit_PP/PBE
+# and store the pseudopotentials in ~/PP/ONCVPSP/Abinit_PP/PBE
 
-INPUT_DIR="../../INPUT_test"
-OUTPUT_DIR="../../LDA"
-functional="LDA"
+INPUT_DIR="../../INPUT"
+OUTPUT_DIR="../../PBE"
+functional="PBE"
 surfix="SG15v1.2"
 echo "Generating PP with ${functional} functional..."
 for file in ${INPUT_DIR}/*1.2.upf
@@ -19,7 +19,7 @@ do
     line_start=$(grep -a --text -n '# ATOM AND REFERENCE CONFIGURATION' $file | awk -F ":" '{print $1}')
     line_end=$(grep -a --text -n '# nvcnf' $file | awk -F ":" '{print $1+1}')
     # echo $line_start $line_end
-    sed -n "$line_start, $line_end p" $file | sed 's/4      upf/3      upf/g' > "./${prefix}.dat"
+    sed -n "$line_start, $line_end p" $file | sed 's/psp8/upf/g' > "./${prefix}.dat"
     ./run.sh ${prefix} -np
     ./extract.sh "${prefix}"
     ./run_r.sh ${prefix} -np
